45237c8df83a7d07d0bf8b1e60ef8313e9cc6de8
kabi1119 <laihei1119@gmail.com>
2025-04-16 21:12:00 +0800

    Migrated account

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,15 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBcZO18FS3lLoNG3MGRqla1qyOCUWypY3A",
-    authDomain: "comp3421-65d4a.firebaseapp.com",
-    projectId: "comp3421-65d4a",
-    storageBucket: "comp3421-65d4a.firebasestorage.app",
-    messagingSenderId: "1027414564286",
-    appId: "1:1027414564286:web:b5857415144cc8622f6def",
-    measurementId: "G-VM6X9DP7PH"
-};
-
-firebase.initializeApp(firebaseConfig);
-console.log("Firebase initialized");
+    apiKey: "AIzaSyDwviP19WkHC_ucfmeAKyvVy4hEcceHfSw",
+    authDomain: "comp3421-2f083.firebaseapp.com",
+    projectId: "comp3421-2f083",
+    storageBucket: "comp3421-2f083.firebasestorage.app",
+    messagingSenderId: "965641466118",
+    appId: "1:965641466118:web:433392e3716a3be85f2e87",
+    measurementId: "G-P7ZCRFHF5M"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
